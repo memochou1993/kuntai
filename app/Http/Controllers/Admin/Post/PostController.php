1b08c76750b37fe8ef6c965a24b336872a6aa7c7
Memo Chou <memochou1993@gmail.com>
2018-09-09 03:55:44 +0800

    修正文章功能

--- app/Http/Controllers/Admin/Post/PostController.php
+++ app/Http/Controllers/Admin/Post/PostController.php
@@ -12,6 +12,11 @@ use App\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
         $agent = new Agent();
--- public/vendor/unisharp/laravel-ckeditor/config.js
+++ public/vendor/unisharp/laravel-ckeditor/config.js
@@ -22,6 +22,7 @@ CKEDITOR.editorConfig = function (config) {
 		{ name: 'others', groups: ['others'] },
 		{ name: 'about', groups: ['about'] }
 	];
+	config.extraPlugins = 'colorbutton';
 	config.removeButtons = 'About';
 	config.format_tags = 'p;h1;h2;h3;pre';
 	config.removeDialogTabs = 'image:advanced;link:advanced';
