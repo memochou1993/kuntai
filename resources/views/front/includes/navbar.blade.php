7456eec0799c4b9b4477d253fc0fc7f32ce717f2
Memo Chou <memochou1993@gmail.com>
2020-02-06 15:13:08 +0000

    Update navbar and footer

--- resources/views/front/includes/navbar.blade.php
+++ resources/views/front/includes/navbar.blade.php
@@ -12,24 +12,28 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item @if (Request::is('/')) {{ 'active' }} @endif">
                     <a href="{{ route('front.home.index') }}" class="nav-link">
-                        首頁
+                       關於 
                     </a>
                 </li>
-                {{-- <li class="nav-item @if (Request::is('items') or Request::is('items*')) {{ 'active' }} @endif">
+                {{--
+                <li class="nav-item @if (Request::is('items') or Request::is('items*')) {{ 'active' }} @endif">
                     <a href="{{ route('front.items.index') }}" class="nav-link">
                         酒款一覽
                     </a>
-                </li> --}}
+                </li>
+                --}}
                 <li class="nav-item @if (Request::is('about')) {{ 'active' }} @endif">
                     <a href="{{ route('front.home.about') }}" class="nav-link">
                         門市資訊
                     </a>
                 </li>
+                {{--
                 <li class="nav-item @if (Request::is('contact')) {{ 'active' }} @endif">
                     <a href="{{ route('front.home.contact') }}" class="nav-link">
                         連絡我們
                     </a>
                 </li>
+                --}}
                 @auth
                     <li class="nav-item">
                         <a href="{{ route('admin.home.index') }}" target="_blank" class="nav-link">
