47f06cd6ef5574c2f40c2bc4609528cce8609746
Memo Chou <memochou1993@gmail.com>
2021-03-19 22:29:45 +0800

    Hide contact button

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -15,10 +15,12 @@
                     <dt class="col-xl-4 text-xl-right">連絡電話</dt>
                     <dd class="col-xl-8">03-5280118</dd>
                 </dl>
+                <!--
                 <hr class="my-4">
                 <div class="text-right">
                     <a class="btn btn-primary btn" href="{{ route('front.home.contact') }}" role="button">聯絡我們</a>
                 </div>
+                -->
             </div>
             <div class="col-xl-8">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.626830711024!2d120.96241771500175!3d24.808228584078666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346835c07b32fbe9%3A0xed13f4368586208f!2z5buj5aSq5LyB5qWt5rSL6KGM!5e0!3m2!1szh-TW!2stw!4v1519309071913" id="map" frameborder="0" style="border:0" allowfullscreen></iframe>
