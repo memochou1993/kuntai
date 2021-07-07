790cb0efc69b8c6f249655ba33de230c25ab5af5
Memo Chou <memochou1993@gmail.com>
2021-07-08 01:00:39 +0800

    Add meta tags

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -2,9 +2,12 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
+<meta name="author" content="Memo Chou">
+<meta name="description" content="新竹最專業的葡萄酒專賣店">
+<meta property="og:type" content="website">
+<meta property="og:title" content="廣太洋酒">
+<meta property="og:description" content="新竹最專業的葡萄酒專賣店">
 <title>廣太洋酒</title>
-
 <link href="{{ asset('images/favicon.png') }}" rel="shortcut icon">
 <link href="{{ asset('css/loading.css') }}" rel="stylesheet">
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
